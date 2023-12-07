0624277673aac9a966a456f95dcea267498f80aa
StijnMichiels00 <stijnmichiels00@gmail.com>
2023-12-07 13:34:50 +0100

    new test

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # import all helper definitions from helper file
 from helper import*
 
-# # Extract all files into a folder called "comm_use_subset"
-# get_files_from_zip('trecc_texts.zip')
+# Extract all files into a folder called "comm_use_subset"
+# get_files_from_zip('test_papers.zip')
 
-# # From every file get a unique ID and text as json
-# convert_to_jsonl("comm_use_subset")
+# From every file get a unique ID and text as json
+# convert_to_jsonl("test_papers")
 
 # Processing text
 # process_text("text_collection.json")
 
-# Creating reversed index
+# # Creating reversed index
 ind = InvertedIndex()
 index(ind, 'clean_text_collection.json')
 ind.write_to_file('test.txt')
